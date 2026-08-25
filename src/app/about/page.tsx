9be00705ce679e1de896cb21e11f4d1740f4ef25
Aman Rastogi <aman@elementtechnologies.com>
2026-08-25 17:46:17 +0530

    feat: add /about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import { AboutHero } from "@/components/AboutHero";
+import { AboutStory } from "@/components/AboutStory";
+import { AboutContact } from "@/components/AboutContact";
+
+export default function AboutPage() {
+  return (
+    <>
+      <AboutHero />
+      <AboutStory />
+      <AboutContact />
+    </>
+  );
+}
---
+++ src/components/AboutHero.tsx
@@ -0,0 +1,14 @@
+export function AboutHero() {
+  return (
+    <section className="mx-auto max-w-6xl px-6 pb-16 pt-14 sm:px-8 sm:pt-20">
+      <span className="font-mono text-[12px] uppercase tracking-widest text-red">
+        About Docly
+      </span>
+      <h1 className="mt-6 text-[3rem] font-black uppercase leading-[0.95] tracking-tight sm:text-6xl lg:text-7xl">
+        We got tired of watching
+        <br />
+        <span className="text-red">layouts break in translation.</span>
+      </h1>
+    </section>
+  );
+}
